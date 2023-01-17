293014e9eb17e82a1c27b7940630294078e61a60
subillie <subinlee@c1r6s7.42seoul.kr>
2023-01-17 23:08:25 +0900

    yeah

--- 컴공실_CSE3013/Week2/Header.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-
-void CountDigits(int N, int *num);
-void Print(int *num);
--- 컴공실_CSE3013/Week2/Print.c
+++
@@ -1,8 +0,0 @@
-#include "Header.h"
-
-void Print(int *num)
-{
-    for (int i = 0; i < 10; i++)
-        printf("%d ", num[i]);
-    printf("\n");
-}
