beb8f4a9eb60936249ebf898815875b959d71bbf
subillie <subinlee@c1r6s2.42seoul.kr>
2023-04-08 05:11:23 +0900

    Friday burning

--- Multicore Programming/Project1_MyShell/prj1_20200422/Phase1/executer.c
+++ Multicore Programming/Project1_MyShell/prj1_20200422/Phase1/executer.c
@@ -1,7 +1,12 @@
 #include "myshell.h"
 
+/* Extern values */
+int signal_flag = 0;
+
 /* Function prototypes */
-static int builtin_command(char **argv);
+static void nonBuiltin(char *filename, char *argv[], char *environ[]);
+static void handler(int sig);
+static int builtinCommand(char **argv);
 
 /* $begin eval */
 /* eval - Evaluate a command line */
@@ -11,17 +16,23 @@ void eval(char *cmdline) {
 	char buf[MAXLINE];		// Holds modified command line
 	int bg;					// Should the job run in bg or fg?
 	pid_t pid;				// Process id
+	sigset_t mask_all, mask_one, prev_one;
+
+	Sigfillset(&mask_all);
+	Sigemptyset(&mask_one);
+	Sigaddset(&mask_one, SIGCHLD);
+	Signal(SIGCHLD, handler);
 
 	bg = parseline(cmdline, buf, argv);
 	if (argv[0] == NULL)	/* Ignore empty lines */
 		return;
 
 	/* quit -> exit(0), & -> ignore, other -> run */
-	if (!builtin_command(argv)) {
-		if (execve(argv[0], argv, environ) < 0) {	// e.g. /bin/ls ls -al &
-			printf("%s: Command not found.\n", argv[0]);
-			exit(0);
-		}
+	if (!builtinCommand(argv)) {
+		char bin_argv[MAXARGS] = "/bin/";
+		strcat(bin_argv, argv[0]);
+		Sigprocmask(SIG_BLOCK, &mask_one, &prev_one);
+		nonBuiltin(argv[0], argv, environ);	// e.g. /bin/ls ls -al &
 
 		/* Parent waits for foreground job to terminate */
 		if (!bg){ 
@@ -32,14 +43,53 @@ void eval(char *cmdline) {
 	return;
 }
 
-/* If opening_q arg is a builtin command, run it and return true */
-static int builtin_command(char **argv) {
+static void nonBuiltin(char *filename, char *argv[], char *environ[]) {
 
-	if (!strcmp(argv[0], "quit"))	// quit command
+	if (execve(filename, argv, environ) < 0) {
+		strcpy(filename, "/bin/");
+		strcat(filename, argv[0]);
+		if (execve(filename, argv, environ) < 0) {
+			strcpy(filename, "/usr/bin/");
+			strcat(filename, argv[0]);
+			if (execve(filename, argv, environ) < 0) {
+				printf("%s: Command not found.\n", argv[0]);
+				exit (0);
+			}
+		}
+	}
+}
+
+/* handler - Handle signal */
+static void handler(int sig) {
+
+	int olderrno = errno;
+	int status;
+	pid_t pid;
+	sigset_t mask_all, prev;
+
+	// WNOHANG, WUNTRACED:
+	// return immediately if no child has exited, and also return if a child has stopped
+	Sigfillset(&mask_all);
+	while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED)) > 0) {
+		Sigprocmask(SIG_BLOCK, &mask_all, &prev);
+		// If child process is stopped or terminated, set signal_flag
+		if(WIFSTOPPED(status) || WIFEXITED(status) || WIFSIGNALED(status))
+			signal_flag = 1;
+		Sigprocmask(SIG_SETMASK, &prev, NULL);
+	}
+	if (errno != ECHILD)
+		unix_error("waitpid error\n");
+	errno = olderrno;
+}
+
+/* If opening_q arg is a builtin command, run it and return true */
+static int builtinCommand(char **argv) {
+
+	if (!strcmp(argv[0], "exit"))		// exit command
 		exit(0);
-	if (!strcmp(argv[0], "&"))		// Ignore singleton &
+	if (!strcmp(argv[0], "&"))			// Ignore singleton &
 		return 1;
-	if (!strcmp(argv[0], "cd")) {	// cd command
+	if (!strcmp(argv[0], "cd")) {		// cd command
 		if (argv[1] == NULL)
 			chdir(getenv("HOME"));
 		else if (chdir(argv[1]) < 0) {
@@ -54,7 +104,45 @@ static int builtin_command(char **argv) {
 		}
 		return 1;
 	}
-	if (!strcmp(argv[0], "echo")) {	// echo command
+	if (!strcmp(argv[0], "mkdir")) {	// mkdir command
+		if (argv[1] == NULL)
+			printf("usage: mkdir [-pv] [-m mode] directory ...\n");
+		else if (mkdir(argv[1], 0755) < 0)
+			printf("mkdir: %s: File exists\n", argv[1]);
+		return 1;
+	}
+	if (!strcmp(argv[0], "rmdir")) {	// rmdir command
+		if (argv[1] == NULL)
+			printf("usage: rmdir [-p] directory ...\n");
+		else if (rmdir(argv[1]) < 0)
+			printf("rmdir: %s: No such file or directory\n", argv[1]);
+		return 1;
+	}
+	if (!strcmp(argv[0], "touch")) {	// touch command
+		if (argv[1] == NULL)
+			printf("usage:\ntouch [-A [-][[hh]mm]SS] [-acfhm] [-r file] [-t [[CC]YY]MMDDhhmm[.SS]] file ...\n");
+		else
+			creat(argv[1], 0644);
+		return 1;
+	}
+	if (!strcmp(argv[0], "cat")) {		// cat command
+		if (argv[1] == NULL)
+			printf("cat: missing operand\n");
+		else {
+			int fd = open(argv[1], O_RDONLY);
+			if (fd < 0) {
+				printf("cat: %s: No such file or directory\n", argv[1]);
+				return 1;
+			}
+			char buf[MAXLINE];
+			int n;
+			while ((n = read(fd, buf, MAXLINE)) > 0)
+				write(1, buf, n);
+			close(fd);
+		}
+		return 1;
+	}
+	if (!strcmp(argv[0], "echo")) {		// echo command
 		int i = 1;
 		while (argv[i] != NULL) {
 			printf("%s ", argv[i]);
@@ -63,6 +151,39 @@ static int builtin_command(char **argv) {
 		printf("\n");
 		return 1;
 	}
-	return 0;						// Not a builtin command
+	if (!strcmp(argv[0], "history")) {
+		if (argv[1] == NULL) {
+			printf("history: missing operand\n");
+			return 1;
+		}
+		else {
+			HISTORY_STATE *history_info = history_get_history_state();
+			HIST_ENTRY **my_history = history_list();
+			for (int i = 0; i < history_info->length; i++) {
+				printf("%d  %s\n", i + 1, my_history[i]->line);
+				free_history_entry(my_history[i]);
+			}
+			free(history_info);
+			free(my_history);
+		}
+	}
+	return 0;							// Not a builtin command
 }
+
+    /* get the state of your history list (offset, length, size) */
+    // HISTORY_STATE *myhist = history_get_history_state ();
+
+    // /* retrieve the history list */
+    // HIST_ENTRY **mylist = history_list ();
+
+    // printf ("\nsession history for %s@%s\n\n", p, host);
+    // for (int i = 0; i < myhist->length; i++) { /* output history list */
+    //     printf (" %8s  %s\n", mylist[i]->line, mylist[i]->timestamp);
+    //     free_history_entry (mylist[i]);     /* free allocated entries */
+    // }
+    // putchar ('\n');
+
+    // free (myhist);  /* free HIST_ENTRY list */
+    // free (mylist);  /* free HISTORY_STATE   */
+
 /* $end eval */
--- Multicore Programming/Project1_MyShell/prj1_20200422/Phase1/myshell.c
+++ Multicore Programming/Project1_MyShell/prj1_20200422/Phase1/myshell.c
@@ -1,19 +1,13 @@
-/* $begin shellmain */
 #include "myshell.h"
 
 /* Function prototypes */
 void handler(int sig);
 
-
+/* $begin shellmain */
 int main() {
 
-	sigset_t mask_all, mask_one;
 	char cmdline[MAXLINE];	/* Command line */
-
-	// Sigfillset(&mask_all);
-	// Sygemptyset(&mask_one);
-	// Sigaddset(&mask_one, SIGCHLD);
-	// Signal(SIGCHLD, handler);
+	using_history();
 
 	while (1) {
 		/* Read */
@@ -24,22 +18,10 @@ int main() {
 
 		/* Evaluate */
 		eval(cmdline);
+
+		if (strlen(cmdline) > 0)
+			add_history(cmdline);
 	}
 	return 0;
 }
 /* $end shellmain */
-
-/* handler - Handle signal */
-void handler(int sig) {
-	// int olderrno = errno;
-	// pid_t pid;
-	// int status;
-
-	// while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-	// 	printf("Job [%d] (%d) terminated by signal %d\n", pid2jid(pid), pid, sig);
-	// 	deletejob(jobs, pid);
-	// }
-	// if (errno != ECHILD)
-	// 	unix_error("waitpid error");
-	// errno = olderrno;
-}
