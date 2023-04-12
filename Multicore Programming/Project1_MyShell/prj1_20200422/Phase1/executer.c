dd6887eeaa23a7e1b3bbd0d2c09fcbcf10dbb9a9
subillie <subinlee@c1r6s3.42seoul.kr>
2023-04-12 22:54:08 +0900

    signal TT

--- Multicore Programming/Project1_MyShell/prj1_20200422/Phase1/executer.c
+++ Multicore Programming/Project1_MyShell/prj1_20200422/Phase1/executer.c
@@ -3,9 +3,12 @@
 /* Global variables */
 int signal_flag = 0;
 int explamation_flag = -1;
+volatile sig_atomic_t pid;
 
 /* Function prototypes */
-static void handler();
+static void handlerSIGINT(int sig);
+static void handlerSIGTSTP(int sig);
+static void handlerSIGCHLD(int sig);
 static void checkExplamation(char **argv);
 static void externFunction(char *filename, char **argv, char **environ);
 static int builtinCommand(char *cmdline, char **argv, FILE *fp_history, int *history_count);
@@ -17,13 +20,15 @@ void eval(char *cmdline, FILE *fp_history, int *history_count) {
 	char *argv[MAXARGS];	// Argument list execve()
 	char buf[MAXLINE];		// Holds modified command line
 	int bg;					// Should the job run in bg or fg?
-	pid_t pid;				// Process id
+	//pid_t pid;				// Process id
 	sigset_t mask_all, mask_one, prev_one;
 
 	Sigfillset(&mask_all);
 	Sigemptyset(&mask_one);
 	Sigaddset(&mask_one, SIGCHLD);
-	Signal(SIGCHLD, handler);
+	Signal(SIGCHLD, handlerSIGCHLD);
+    Signal(SIGINT, handlerSIGINT);
+    Signal(SIGTSTP, handlerSIGTSTP);
 
 	// Parse command line
 	bg = parseline(cmdline, buf, argv);
@@ -55,46 +60,65 @@ void eval(char *cmdline, FILE *fp_history, int *history_count) {
 			Sigprocmask(SIG_SETMASK, &prev_one, NULL);
 			setpgid(0, 0);
 			externFunction(filename, argv, environ);
+            exit(1);
 		}
-		int status;
-		wait(&status);
 		Sigprocmask(SIG_BLOCK, &mask_all, NULL);
 		Sigprocmask(SIG_SETMASK, &prev_one, NULL);
 	}
 	// Parent waits for foreground job to terminate
 	if (!bg) {
 		int status;
-		// if (waitpid(pid, &status, 0) < 0)
-		// 	unix_error("waitfg: waitpid error");
+		 if (waitpid(pid, &status, 0) < 0)
+		 	unix_error("waitfg: waitpid error");
 	}
 	else	// when there is background process
 		printf("%d %s", pid, cmdline);
 	return;
 }
 
-/* handler - Handle signal */
-static void handler() {
+static void handlerSIGINT(int sig) {
 
-	int olderrno = errno;
-	// int status;
-	// pid_t pid;
-	// sigset_t mask_all, prev;
-
-	// Sigfillset(&mask_all);
-	// // Wait for a child process to stop or terminate
-	// while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED))) {
-	// 	Sigprocmask(SIG_BLOCK, &mask_all, &prev);
-
-	// 	// If child process is stopped or terminated, set signal_flag
-	// 	if(WIFSTOPPED(status) || WIFEXITED(status) || WIFSIGNALED(status))
-	// 		signal_flag = 1;
-
-	// 	Sigprocmask(SIG_SETMASK, &prev, NULL);
-	// }
-	// if (errno != ECHILD)	//  ECHILD - No child processes (POSIX.1-2001).
-	// 	unix_error("waitpid error");
-	errno = olderrno;
+    if (pid != 0)
+        kill(-pid, SIGINT);
+    return ;
 }
+
+static void handlerSIGTSTP(int sig) {
+
+    if (pid != 0)
+        kill(-pid, SIGTSTP);
+    return ;
+}
+
+static void handlerSIGCHLD(int sig) {
+    int olderrno = errno;
+    pid = waitpid(-1, NULL, 0);
+    errno = olderrno;
+}
+
+/* handler - Handle signal */
+//static void handler() {
+//
+//    int olderrno = errno;
+//    int status;
+//    pid_t pid;
+//    sigset_t mask_all, prev;
+//
+//    Sigfillset(&mask_all);
+//    // Wait for a child process to stop or terminate
+//    while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED))) {
+//       Sigprocmask(SIG_BLOCK, &mask_all, &prev);
+//
+//       // If child process is stopped or terminated, set signal_flag
+//       if(WIFSTOPPED(status) || WIFEXITED(status) || WIFSIGNALED(status))
+//           signal_flag = 1;
+//
+//       Sigprocmask(SIG_SETMASK, &prev, NULL);
+//    }
+//    if (errno != ECHILD)	//  ECHILD - No child processes (POSIX.1-2001).
+//       unix_error("waitpid error");
+//    errno = olderrno;
+//}
 
 /* checkExplamation - Check if there is !! or !# */
 static void checkExplamation(char **argv) {
