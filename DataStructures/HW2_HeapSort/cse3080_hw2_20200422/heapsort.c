823b3b6ed71c53bf0d4adc20dd3012c11336fba7
subillie <subinlee@c1r7s6.42seoul.kr>
2023-05-11 21:10:55 +0900

    [DS] done w/ heap sort

--- DataStructures/HW2_HeapSort/cse3080_hw2_20200422/heapsort.c
+++ DataStructures/HW2_HeapSort/cse3080_hw2_20200422/heapsort.c
@@ -1,17 +1,35 @@
-#include "heapsort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <time.h>
+
+#define MAX_LEN 17 /* max length of commands + max size of integer + 1 */
+#define MAX_ELEMENTS 1000000
+#define HEAP_FULL(n) (n == MAX_ELEMENTS - 1)
+#define HEAP_EMPTY(n) (!n)
+
+/* global variables */
+typedef struct {
+	int key;
+} element;
+
+element heap[MAX_ELEMENTS];
+int n = 0;
 
 /* funcion prototypes */
 void *Malloc(size_t size);
 void insert_max_heap(element item, int *n);
-element delete_max_heap(int *n, element *tmpHeap);
-element delete_min_heap(int *n, element *tmpHeap, int count);
+void insert_min_heap(element *tmpHeap, element tmpItem, int *tmpN);
+element delete_max_heap(int *tmpN, element *tmpHeap);
+element delete_min_heap(int *tmpN, element *tmpHeap);
 
 /* main */
 int main(int ac, char **av) {
 
-	int i, newItem, count = 0;
+	int i, newItem, tmpN;
 	char *line = (char *)Malloc(sizeof(char) * (MAX_LEN + 1));
-	element item, tmpItem;
+	element item, tmpItem, tmpHeap[MAX_ELEMENTS];
 	clock_t start, end;
 
 	/* If the number of command-line arguments is not 1 */
@@ -32,26 +50,26 @@ int main(int ac, char **av) {
 	start = clock();
 	/* read lines from the input file, and execute the commands */
 	while (fgets(line, MAX_LEN, fp_in)) {
-		/* insert */
-		if (line[0] == 'I') {
+		if (line[0] == 'I') {        /* insert */
 			newItem = atoi(line + 7);
 			item.key = newItem;
 			insert_max_heap(item, &n);
-			count++;
-		} else {
-			element tmpHeap[MAX_ELEMENTS];
-			for (i = 1; i <= count; i++) { tmpHeap[i] = heap[i]; }
-			/* ascend */
-			if (line[0] == 'A') {
-				for (i = 1; i <= count; i++) {
-					tmpItem = delete_max_heap(&n, tmpHeap);
-					fprintf(fp_out, "%d ", tmpItem.key);
-				}
-			/* descend */
-			} else {
-				tmpItem = delete_min_heap(&n, tmpHeap, count);
-				for (i = 1; i <= count; i++) { fprintf(fp_out, "%d ", tmpItem.key); }
+		} else if (line[0] == 'A') { /* ascend */
+			tmpN = 0;
+			for (i = 1; i <= n; i++) { insert_min_heap(tmpHeap, heap[i], &tmpN); }
+			for (i = 1; i <= n; i++) {
+				tmpItem = delete_min_heap(&tmpN, tmpHeap);
+				fprintf(fp_out, "%d ", tmpItem.key);
 			}
+			fprintf(fp_out, "\n");
+		} else {                     /* descend */
+			tmpN = n;
+			for (i = 1; i <= n; i++) { tmpHeap[i] = heap[i]; }
+			for (i = 1; i <= n; i++) {
+				tmpItem = delete_max_heap(&tmpN, tmpHeap);
+				fprintf(fp_out, "%d ", tmpItem.key);
+			}
+			fprintf(fp_out, "\n");
 		}
 	}
 	end = clock();
@@ -67,7 +85,9 @@ int main(int ac, char **av) {
 /* insert item into a max heap of current size *n */
 void insert_max_heap(element item, int *n) {
 
-	if (HEAP_FULL(*n)) { fprintf(stderr, "The heap is full.\n"); exit(1); }
+	if (HEAP_FULL(*n)) {
+		fprintf(stderr, "The heap is full.\n"); exit(1);
+	}
 
 	int i = ++(*n);
 	while ((i != 1) && (item.key > heap[i / 2].key)) {
@@ -77,20 +97,32 @@ void insert_max_heap(element item, int *n) {
 	heap[i] = item;
 }
 
+/* insert item into a min heap of current size n */
+void insert_min_heap(element *tmpHeap, element tmpItem, int *tmpN) {
+
+	int i = ++(*tmpN);
+	while ((i != 1) && (tmpItem.key < tmpHeap[i / 2].key)) {
+		tmpHeap[i] = tmpHeap[i / 2];
+		i /= 2;
+	}
+	tmpHeap[i] = tmpItem;
+}
 
 /* delete element with the highest key from the heap */
-element delete_max_heap(int *n, element *tmpHeap) {
+element delete_max_heap(int *tmpN, element *tmpHeap) {
 
-	if(HEAP_EMPTY(*n)) { fprintf(stderr, "The heap is empty"); exit(1); }
+	if (HEAP_EMPTY(*tmpN)) {
+		fprintf(stderr, "The heap is empty"); exit(1);
+	}
 	/* save value of the element with the largest key */
 	element item = tmpHeap[1];
 	/* use the last element in the heap to adjust heap */
-	element temp = tmpHeap[(*n)--];
+	element temp = tmpHeap[(*tmpN)--];
 
 	int parent = 1, child = 2;
-	while(child <= *n) {
+	while (child <= *tmpN) {
 		/* find the larger child of the current parent */
-		if((child < *n) && (tmpHeap[child].key < tmpHeap[child+1].key)) child++;
+		if((child < *tmpN) && (tmpHeap[child].key < tmpHeap[child + 1].key)) child++;
 		if(temp.key >= tmpHeap[child].key) break;
 		/* move to the next lower level */
 		tmpHeap[parent] = tmpHeap[child];
@@ -101,20 +133,22 @@ element delete_max_heap(int *n, element *tmpHeap) {
 	return item;
 }
 
-/* delete element with the lowest key from the heap */
-element delete_min_heap(int *n, element *tmpHeap, int count) {
+/* delete element with the highest key from the heap */
+element delete_min_heap(int *tmpN, element *tmpHeap) {
 
-	if(HEAP_EMPTY(*n)) { fprintf(stderr, "The heap is empty"); exit(1); }
+	if (HEAP_EMPTY(*tmpN)) {
+		fprintf(stderr, "The heap is empty"); exit(1);
+	}
 	/* save value of the element with the largest key */
 	element item = tmpHeap[1];
 	/* use the last element in the heap to adjust heap */
-	element temp = tmpHeap[(*n)--];
+	element temp = tmpHeap[(*tmpN)--];
 
 	int parent = 1, child = 2;
-	while(child <= *n) {
+	while (child <= *tmpN) {
 		/* find the larger child of the current parent */
-		if((child < *n) && (tmpHeap[child].key < tmpHeap[child+1].key)) child++;
-		if(temp.key >= tmpHeap[child].key) break;
+		if((child < *tmpN) && (tmpHeap[child].key > tmpHeap[child + 1].key)) child++;
+		if(temp.key <= tmpHeap[child].key) break;
 		/* move to the next lower level */
 		tmpHeap[parent] = tmpHeap[child];
 		parent = child;
@@ -129,7 +163,7 @@ void *Malloc(size_t size) {
 
 	void *ptr = malloc(size);
 	if (!ptr) {
-		fprintf(stderr, "Memory allocation failed.\n");
+		fprintf(stderr, "error: memory allocation failed.\n");
 		exit(1);
 	}
 	return ptr;
